927af75d12a1af1574d9fbb5f708747b65faf1a4
thughari <thughari3@gmail.com>
2021-12-11 07:22:55 +0530

    listening

--- ai.py
+++ ai.py
@@ -17,7 +17,7 @@ try:
     with sr.Microphone() as source:
         engine.say('i am listening now...speak anything')
         engine.runAndWait()
-        print("i am listening now...................................................")
+        print('i am listening now...................................................')
         audio=r.listen(source)
     engine.say(audio)
     txt=r.recognize_google(audio)
